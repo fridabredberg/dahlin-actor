f13791022e6bc361d372698d71eb12dbb470a8c7
Frida Bredberg <fridabredberg@26.234.181.62.in-addr.dgcsystems.net>
2021-06-17 13:11:29 +0200

    Add filters

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -15,6 +15,8 @@ export class TodoListComponent {
     { title: 'laugh', checked: false },
   ];
 
+  filter: 'all' | 'active' | 'done' = 'all';
+
   @Input() newItem = {};
   
   addNewTodo(input: any) {
@@ -22,6 +24,13 @@ export class TodoListComponent {
       title: input,
       checked: false
     });
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    else return this.allItems.filter(item => this.filter === 'done' ? item.checked : !item.checked)
   }
 
 }
